4dfa3b727bfdb8e5f70c6b2a40b3823663a14011
Mathew Torres <torres.mathew@gmail.com>
2022-11-14 14:43:57 -0700

    initial commit

---
+++ src/main/java/com/mtorres6739codefellowship/mtorres6739codefellowship/Mtorres6739codefellowshipApplication.java
@@ -0,0 +1,13 @@
+package com.mtorres6739codefellowship.mtorres6739codefellowship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mtorres6739codefellowshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mtorres6739codefellowshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtorres6739codefellowship/mtorres6739codefellowship/Mtorres6739codefellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtorres6739codefellowship.mtorres6739codefellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mtorres6739codefellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
